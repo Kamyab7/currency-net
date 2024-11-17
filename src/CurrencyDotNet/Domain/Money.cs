5b9192afc9429b1577d5c45a4c0d233b748119de
imun <imun22@gmail.com>
2024-11-17 16:54:14 +0100

    add Currency Name & Symbol for Money struct

--- src/CurrencyDotNet/Domain/Money.cs
+++ src/CurrencyDotNet/Domain/Money.cs
@@ -14,14 +14,28 @@ public struct Money
     /// The Currency ISO Code
     /// </summary>
     public string CurrencyCode { get; }
+    
+    /// <summary>
+    /// Currency Symbol like $ for USD
+    /// </summary>
+    public string CurrencySymbol { get; }
+    
+    /// <summary>
+    /// Name of the Currency (ex: Us Dollar for USD)
+    /// </summary>
+    public string CurrencyName { get; }
 
     public Money(decimal amount, string currencyCode)
     {
         Amount = amount;
-        if (CurrencySource.FindByCode(currencyCode) is null)
+        var currency = CurrencySource.FindByCode(currencyCode); 
+        if (currency is null)
         {
             throw new KeyNotFoundException($"Currency with '{currencyCode}' not found");
         }
         CurrencyCode = currencyCode;
+        CurrencyName = currency.Name;
+        CurrencySymbol = currency.Symbol;
     }
+    
 }
