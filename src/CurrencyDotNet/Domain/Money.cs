dacfd581d0649c190aee3c8de90dce731c128856
imun <imun22@gmail.com>
2024-11-17 15:55:59 +0100

    add intial Money struct

---
+++ src/CurrencyDotNet/Domain/Money.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace CurrencyDotNet;
+
+public struct Money
+{
+    
+    /// <summary>
+    /// The amount of the Money
+    /// </summary>
+    public decimal Amount { get; }
+    
+    /// <summary>
+    /// The Currency ISO Code
+    /// </summary>
+    public string CurrencyCode { get; }
+
+    public Money(decimal amount, string currencyCode)
+    {
+        Amount = amount;
+        if (CurrencySource.FindByCode(currencyCode) is null)
+        {
+            throw new KeyNotFoundException($"Currency with '{currencyCode}' not found");
+        }
+    }
+}
