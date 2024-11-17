a581c9b9b851c5d55c43fe15b7ff2772fc87cf6e
imun <imun22@gmail.com>
2024-11-17 16:59:29 +0100

    add setFields method

--- src/CurrencyDotNet/Domain/Money.cs
+++ src/CurrencyDotNet/Domain/Money.cs
@@ -1,31 +1,42 @@
 using System.Collections.Generic;
+// ReSharper disable CheckNamespace
 
 namespace CurrencyDotNet;
 
+/// <summary>
+/// Money type with support for a standard <see cref="Currency"/>
+/// </summary>
 public struct Money
 {
     
     /// <summary>
     /// The amount of the Money
     /// </summary>
-    public decimal Amount { get; }
+    public decimal Amount { get; private set; }
     
     /// <summary>
     /// The Currency ISO Code
     /// </summary>
-    public string CurrencyCode { get; }
+    public string CurrencyCode { get; private set; }
     
     /// <summary>
     /// Currency Symbol like $ for USD
     /// </summary>
-    public string CurrencySymbol { get; }
+    public string CurrencySymbol { get; private set; }
     
     /// <summary>
     /// Name of the Currency (ex: Us Dollar for USD)
     /// </summary>
-    public string CurrencyName { get; }
+    public string CurrencyName { get; private set; }
 
+#pragma warning disable CS8618, CS9264
     public Money(decimal amount, string currencyCode)
+#pragma warning restore CS8618, CS9264
+    {
+        setFields(amount, currencyCode);
+    }
+    
+    private void setFields(decimal amount, string currencyCode)
     {
         Amount = amount;
         var currency = CurrencySource.FindByCode(currencyCode); 
